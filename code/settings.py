6847bb786d98fac2dfb3bb8ab164f410d664c86b
dnt0n <107097221+dnt0n@users.noreply.github.com>
2024-07-04 23:37:28 +0800

    Update settings.py

--- code/settings.py
+++ code/settings.py
@@ -4,7 +4,7 @@ WINDOW_HEIGHT = 800
 FRAMERATE = 120
 
 # adjust game speed
-OBSTACLE_INTERVAL = 1300 # spawning speed of obstacles in ms
+OBSTACLE_INTERVAL = 1300 # spawning interval of obstacles in ms
 OBSTACLE_SPEED = 250
 GROUND_SPEED = OBSTACLE_SPEED
 BACKGROUND_SPEED = GROUND_SPEED * 0.8
@@ -16,11 +16,5 @@ JUMP_HEIGHT = 400
 # adjust obstacle dimensions
 OBSTACLE_SCALE_FACTOR = 1
 
-# adjust difficulty levels
+# adjust difficulty levels (easy, medium, hard)
 GAME_SPEED_FACTORS = [1, 1.5, 2]
-
-'''
-TO DO:
-- add a new obstacle type similar to that in flappy bird
-- implement 3 difficulty tiers as time passes: easy (<= 15) - medium (16 - 50) - hard (>= 51)
-'''
